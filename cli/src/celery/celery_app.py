30090c6d8ff07485eb3c5e88a28f498e94afce5f
Kiyoteru Hosoda <kiyoteru.hosoda@gmail.com>
2025-08-22 15:27:54 +0900

    celery連携に

--- cli/src/celery/celery_app.py
+++ cli/src/celery/celery_app.py
@@ -1,10 +1,16 @@
 from celery import Celery
+from flask import Flask
+from webapp.config import Config
 
-def make_celery(app_name=__name__):
-    return Celery(
-        app_name,
+def make_celery(app: Flask):
+    celery = Celery(
+        app.import_name,
         broker="redis://localhost:6379/0",  # Broker
         backend="redis://localhost:6379/0"  # 結果を取得する場合
     )
+    celery.conf.update(app.config)
+    return celery
 
-celery = make_celery()
+app = Flask(__name__)
+app.config.from_object(Config)
+celery = make_celery(app)

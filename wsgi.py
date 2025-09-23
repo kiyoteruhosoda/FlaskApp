85b3f133420ffa9effe6d50472082a9765624f66
Kiyoteru Hosoda <kiyoteru.hosoda@gmail.com>
2025-09-23 18:43:00 +0900

    Migrationのときの問題解決

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,10 @@ from core.db_log_handler import DBLogHandler
 
 app = create_app()
 
-from datetime import datetime, timezone
-
-with app.app_context():
-    now = datetime.now(timezone.utc).isoformat()
-    app.logger.info(f"Start Flask web app: wsgi.py {now}", extra={"event": "app.startup"})
-
 if __name__ == "__main__":
+    from datetime import datetime, timezone
+    with app.app_context():
+        now = datetime.now(timezone.utc).isoformat()
+        app.logger.info(f"Start Flask web app: wsgi.py {now}", extra={"event": "app.startup"})
+
     app.run(debug=True)
